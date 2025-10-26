5f4fc601c95f067697834c5189ccc1dbdd9681e5
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2025-10-26 01:44:36 -0600

    Create UserUnitTest.java

---
+++ Skeddly/app/src/androidTest/java/com/example/skeddly/UserUnitTest.java
@@ -0,0 +1,29 @@
+package com.example.skeddly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
+import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.skeddly.business.database.DatabaseHandler;
+import com.example.skeddly.business.user.Authenticator;
+import com.example.skeddly.business.user.User;
+import com.example.skeddly.business.user.UserLoaded;
+
+@RunWith(AndroidJUnit4.class)
+public class UserUnitTest {
+    @Test
+    public void testLoginUser() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        DatabaseHandler database = new DatabaseHandler(appContext);
+        Authenticator authenticator = new Authenticator(appContext, database, new UserLoaded() {
+            @Override
+            public void onUserLoaded(User loadedUser) {
+                assertNotNull(loadedUser);
+            }
+        });
+    }
+}
