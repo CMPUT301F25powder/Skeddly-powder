0afa7d5c4bf6587365b8bf197a7b3c122309b6fc
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2025-10-26 21:40:37 -0600

    fixed crash  / fixed ids

--- Skeddly/app/src/main/java/com/example/skeddly/ui/PersonalInformationEditFragment.java
+++ Skeddly/app/src/main/java/com/example/skeddly/ui/PersonalInformationEditFragment.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.skeddly.R;
 import com.example.skeddly.databinding.PersonalInformationEditFragmentBinding;
 
 
@@ -26,13 +24,6 @@ public class PersonalInformationEditFragment extends Fragment {
         return root;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        TextView headerTitle = binding.profileHeader.profileTitle;
-        headerTitle.setText(R.string.personal_information);
-    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
