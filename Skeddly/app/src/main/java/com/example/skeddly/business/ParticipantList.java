aeda37f24cd3a68c00ff49d365b160ae5e82db73
Jonathan Bonham <jonathanwbon+github@protonmail.com>
2025-10-28 22:42:12 -0600

    Finalize list classes

--- Skeddly/app/src/main/java/com/example/skeddly/business/ParticipantList.java
+++ Skeddly/app/src/main/java/com/example/skeddly/business/ParticipantList.java
@@ -1,29 +1,69 @@
 package com.example.skeddly.business;
 
+import com.example.skeddly.business.user.User;
+
 import java.util.ArrayList;
 
+/**
+ * Class for list of participants in an event.
+ */
 public class ParticipantList {
     private final ArrayList<User> userList = new ArrayList<User>();
     private int maxAttend;
 
+    /**
+     * Constructor for unlimited length participant list.
+     */
     public ParticipantList() {
         maxAttend = Integer.MAX_VALUE;
     }
+
+    /**
+     * Constructor for constrained participant list
+     * @param maxAttend Maximum amount of people that can attend the event.
+     */
     public ParticipantList(int maxAttend) {
         this.maxAttend = maxAttend;
     }
 
+    /**
+     * Add a user to the participant list
+     * @param u User to add to the list
+     */
     public void addUser(User u) {
-        userList.add(u);
+        if (userList.size() < maxAttend) {
+            userList.add(u);
+        }
     }
+
+    /**
+     * Remove a user from the list
+     * @param u User to remove from the list
+     */
     public void remove(User u) {
         userList.remove(u);
     }
 
+    /**
+     * Find the maximum amount of people that are allowed in the list
+     * @return Maximum number of attendees
+     */
     public int getMaxAttend() {
         return maxAttend;
     }
 
+    /**
+     * Update the maximum number of attendees
+     * @param maxAttend The new maximum amount of attendees
+     */
+    void setMaxAttend(int maxAttend) {
+        this.maxAttend = maxAttend;
+    }
+
+    /**
+     * Return the {@link ArrayList} of users
+     * @return The user list
+     */
     public ArrayList<User> get() {
         return userList;
     }
--- Skeddly/app/src/main/java/com/example/skeddly/business/WaitingList.java
+++ Skeddly/app/src/main/java/com/example/skeddly/business/WaitingList.java
@@ -1,36 +1,84 @@
 package com.example.skeddly.business;
 
+import com.example.skeddly.business.user.User;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class for list of people who want to attend an event
+ */
 public class WaitingList {
     private final ArrayList<Ticket> ticketList = new ArrayList<Ticket>();
     private int maxWait;
+    private Random randomGen;
 
+    /**
+     * Constructor for unbounded waiting list
+     */
     public WaitingList() {
         maxWait = Integer.MAX_VALUE;
+        randomGen = new Random();
     }
+
+    /**
+     * Constructor for bounded waiting list
+     * @param maxWait Maximum number of applicants
+     */
     public WaitingList(int maxWait) {
         this.maxWait = maxWait;
+        randomGen = new Random();
     }
+
+    /**
+     * Add a ticket to the waiting list
+     * @param t Ticket to add
+     */
     public void addTicket(Ticket t) {
-        ticketList.add(t);
+        if (ticketList.size() < maxWait) {
+            ticketList.add(t);
+        }
     }
+
+    /**
+     * Remove a user from the waiting list
+     * @param u User to remove
+     */
     public void remove(User u) {
         ticketList.removeIf(t -> t.getUser().equals(u));
     }
+
+    /**
+     * Remove a ticket from the waiting list
+     * @param t Ticket to remove
+     */
     public void remove(Ticket t) {
         ticketList.remove(t);
     }
+
+    /**
+     * Randomly select and remove a ticket from the waiting list
+     * @return The selected ticket
+     */
     public Ticket draw() {
-        Ticket t = ticketList.get(Random.nextInt(ticketList.size()));
+        Ticket t = ticketList.get(randomGen.nextInt(ticketList.size()));
         this.remove(t);
 
         return t;
     }
+
+    /**
+     * Return the maximum number of people allowed in the waiting list
+     * @return The maximum length of the list
+     */
     public int getLimit() {
         return maxWait;
     }
+
+    /**
+     * Set the maximum number of people allowed in the waiting list
+     * @param maxWait The new limit
+     */
     public void setLimit(int maxWait) {
         this.maxWait = maxWait;
     }
