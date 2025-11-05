2d56653187ed9d290bab9defdcbab0866fe3d105
Abbas <abbas.rizvi20089@gmail.com>
2025-11-04 22:38:58 -0700

    Fragment to inflate entrant list

---
+++ Skeddly/app/src/main/java/com/example/skeddly/ui/ParticipantListFragment.java
@@ -0,0 +1,34 @@
+package com.example.skeddly.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.skeddly.databinding.EntrantListViewBinding;
+
+public class ParticipantListFragment extends Fragment {
+
+    private EntrantListViewBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment using View Binding
+        binding = EntrantListViewBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Set the binding to null to avoid memory leaks
+        binding = null;
+    }
+}
