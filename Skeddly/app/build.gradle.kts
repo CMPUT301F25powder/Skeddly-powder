f6752b0becc7e8250119d89cb05dd24c0c55be6c
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2025-11-06 21:57:19 -0700

    implemented search functionality

--- Skeddly/app/build.gradle.kts
+++ Skeddly/app/build.gradle.kts
@@ -69,6 +69,10 @@ dependencies {
     // Maps SDK for Android
     implementation("com.google.android.gms:play-services-maps:19.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    // Other math
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-text
+    implementation("org.apache.commons:commons-text:1.14.0")
 }
 
 secrets {
--- Skeddly/app/src/main/java/com/example/skeddly/ui/HomeFragment.java
+++ Skeddly/app/src/main/java/com/example/skeddly/ui/HomeFragment.java
@@ -1,10 +1,18 @@
 package com.example.skeddly.ui;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,16 +28,19 @@ import com.example.skeddly.business.database.DatabaseObjects;
 import com.google.firebase.auth.FirebaseAuth;
 
 
+import org.apache.commons.text.similarity.LevenshteinDetailedDistance;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+
 import java.util.ArrayList;
 import java.util.Objects;
 
 
 public class HomeFragment extends Fragment {
     private HomeFragmentBinding binding;
-    private ArrayList<Event> eventList;
+    private ArrayList<Event> eventList = new ArrayList<>();
     private DatabaseHandler databaseHandler;
     private EventAdapter eventAdapter;
-
+    private SimpleCursorAdapter simpleCursorAdapter;
 
     @Nullable
     @Override
@@ -39,7 +50,6 @@ public class HomeFragment extends Fragment {
 
         // Initialize DatabaseHandler and list of events
         databaseHandler = new DatabaseHandler();
-        eventList = new ArrayList<>();
 
         // Initialize event adapter
         MainActivity mainActivity = (MainActivity) requireActivity();
@@ -51,10 +61,85 @@ public class HomeFragment extends Fragment {
         // Fetch events from firebase
         fetchEvents();
 
+        final String[] from = new String[] {"eventName"};
+        final int[] to = new int[] {android.R.id.text1};
+
+        SearchView searchBar = binding.searchEvents;
+        simpleCursorAdapter = new SimpleCursorAdapter(root.getContext(), android.R.layout.simple_list_item_1, null, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        searchBar.setSuggestionsAdapter(simpleCursorAdapter);
+
+        searchBar.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+            @Override
+            public boolean onSuggestionClick(int position) {
+                Cursor cursor = (Cursor) simpleCursorAdapter.getItem(position);
+                String txt = cursor.getString(cursor.getColumnIndexOrThrow("eventName"));
+                searchBar.setQuery(txt, true);
+                return true;
+            }
+
+            @Override
+            public boolean onSuggestionSelect(int position) {
+                return true;
+            }
+        });
+
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.w("NEW TEXT", newText);
+
+                if (Objects.equals(newText, "")) {
+                    fetchEvents();
+                } else {
+                    populateAdapter(newText);
+                }
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchEvents(query);
+                populateAdapter(query);
+                searchBar.clearFocus();
+
+                return true;
+            }
+        });
+
         return root;
     }
 
-    private void fetchEvents() {
+    private boolean checkNameSuggestionMatch(String name, String query) {
+        if (query.isBlank()) {
+            return true;
+        }
+
+        LevenshteinDistance levenshteinDistance = LevenshteinDistance.getDefaultInstance();
+
+        int distance = levenshteinDistance.apply(name.toLowerCase(), query.toLowerCase());
+
+        return distance <= 2;
+    }
+
+    private void populateAdapter(String query) {
+        final MatrixCursor c = new MatrixCursor(new String[]{ BaseColumns._ID, "eventName" });
+        for (int i = 0; i < eventList.size(); i++) {
+            Event event = eventList.get(i);
+            String eventName = event.getEventDetails().getName();
+
+            int clampedEndNameIndex = Math.min(query.length(), eventName.length());
+            String queryLengthName = eventName.substring(0, clampedEndNameIndex);
+
+            if (checkNameSuggestionMatch(queryLengthName, query))
+                c.addRow(new Object[] {i, eventName});
+        }
+
+        simpleCursorAdapter.changeCursor(c);
+    }
+
+    private void fetchEvents(String query) {
         // Fetch events from firebase
         databaseHandler.iterableListen(databaseHandler.getEventsPath(),
                 Event.class,
@@ -64,7 +149,12 @@ public class HomeFragment extends Fragment {
 
                     // Add new events to list
                     for (int i = 0; i < dbObjects.size(); i++) {
-                        eventList.add((Event) dbObjects.get(i));
+                        Event event = (Event) dbObjects.get(i);
+                        String eventName = event.getEventDetails().getName();
+
+                        if (checkNameSuggestionMatch(eventName, query)) {
+                            eventList.add(event);
+                        }
                     }
 
                     // Notify adapter of changes
@@ -72,6 +162,10 @@ public class HomeFragment extends Fragment {
 
                 }
         );
+    }
+
+    private void fetchEvents() {
+        fetchEvents("");
     }
 
     @Override
