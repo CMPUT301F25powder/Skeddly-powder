36b6b01801d37c3ae22b0d826abf7ffb4f14e5eb
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2025-11-06 23:47:48 -0700

    Create DatabaseObjectsUnitTest.java

---
+++ Skeddly/app/src/test/java/com/example/skeddly/business/DatabaseObjectsUnitTest.java
@@ -0,0 +1,28 @@
+package com.example.skeddly.business;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.skeddly.business.database.DatabaseObject;
+import com.example.skeddly.business.database.DatabaseObjects;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DatabaseObjectsUnitTest {
+    @Test
+    public void testDatabaseObjectsIds() {
+        DatabaseObjects databaseObjects = new DatabaseObjects();
+
+        DatabaseObject obj1 = new DatabaseObject();
+        DatabaseObject obj2 = new DatabaseObject();
+        DatabaseObject obj3 = new DatabaseObject();
+
+        databaseObjects.addAll(Arrays.asList(obj1, obj2, obj3));
+
+        ArrayList<String> testIds = new ArrayList<>(Arrays.asList(obj1.getId(), obj2.getId(), obj3.getId()));
+
+        assertEquals(testIds, databaseObjects.getIds());
+    }
+}
