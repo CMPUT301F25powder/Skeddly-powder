85d18b50e748b687ef15e9954818447e30c5fc9b
Adam Mehaney <59457164+adammehaney@users.noreply.github.com>
2025-11-24 02:59:35 -0700

    fixed image cropping

--- Skeddly/app/src/main/java/com/example/skeddly/ui/adapter/GalleryImageAdapter.java
+++ Skeddly/app/src/main/java/com/example/skeddly/ui/adapter/GalleryImageAdapter.java
@@ -17,6 +17,8 @@ import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.skeddly.R;
 import com.example.skeddly.ui.utils.GalleryImage;
 
